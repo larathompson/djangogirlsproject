ee14a57429b139105810a9c44946f70c8ddfb734
larathompson <larathompson288@gmail.com>
2020-12-20 13:25:20 +0000

    html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+  #this returns the value it gets from calling another functoin called render
+  return render(request, 'blog/post_list.html', {})
